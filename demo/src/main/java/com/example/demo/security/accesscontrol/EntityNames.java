f543bb8b05e8032c8efc3b949e5dfb746645fb77
Harley Diniz <fhdfm85@gmail.com>
2025-02-24 13:30:42 -0300

    finalizando.

--- demo/src/main/java/com/example/demo/security/accesscontrol/EntityNames.java
+++ demo/src/main/java/com/example/demo/security/accesscontrol/EntityNames.java
@@ -3,9 +3,15 @@ package com.example.demo.security.accesscontrol;
 public final class EntityNames {
     
     public static final String ESCOLA = "escola";
+    
     public static final String USUARIO = "usuario";
+    
     public static final String ALUNO = "aluno";
-    public static final String PRODUTO = "professor";
+    
+    public static final String PRODUTO = "produto";
+    
     public static final String CARTAO = "cartao";
+
+    public static final String PEDIDO = "pedido";
 
 }
