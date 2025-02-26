81f003ca9191065c09ff275eb4b1cb7f18c29c36
igoredm <igoredm@gmail.com>
2025-02-26 11:27:46 -0300

    Add coluna nova

--- demo/database.sql
+++ demo/database.sql
@@ -12,6 +12,7 @@ CREATE TABLE escola (
     nome VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) NOT NULL UNIQUE,
     status VARCHAR(20) CHECK (status IN ('ATIVO', 'INATIVO')) DEFAULT 'ATIVO',
+    payment_secret VARCHAR(255) UNIQUE,
     version INT NOT NULL DEFAULT 0,
     criado_em TIMESTAMP DEFAULT NOW()
 );
