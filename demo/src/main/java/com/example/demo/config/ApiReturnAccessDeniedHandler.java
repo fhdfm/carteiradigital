2c4fa88ef2a3f19c878e2cfe8dd90767dd0f2278
igoredm <igoredm@gmail.com>
2025-03-04 20:00:13 -0300

    Delete import

--- demo/src/main/java/com/example/demo/config/ApiReturnAccessDeniedHandler.java
+++ demo/src/main/java/com/example/demo/config/ApiReturnAccessDeniedHandler.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
