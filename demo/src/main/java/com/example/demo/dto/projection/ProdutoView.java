043e625239d2b681ddb4ac03f13df6aaf53822ef
igoredm <igoredm@gmail.com>
2025-05-22 23:06:23 -0300

    Teste Produto

--- demo/src/main/java/com/example/demo/dto/projection/ProdutoView.java
+++ demo/src/main/java/com/example/demo/dto/projection/ProdutoView.java
@@ -4,12 +4,15 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import com.example.demo.domain.enums.Departamento;
+import com.example.demo.dto.projection.produto.CategoriaSummary;
 
 public interface ProdutoView {
 
     UUID getUuid();
 
     String getNome();
+
+    CategoriaSummary getCategoria();
 //
 //    String getFoto();
 //
